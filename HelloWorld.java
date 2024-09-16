378af13ceedfae284d7eb960d2b43fd3c3d14b5b
Luis Santos Ayala <luis.santosayala@gmail.com>
2024-09-16 12:16:52 -0400

    Create HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+
+public class HelloWorld
+{
+   
+   public static void main(String[] Args)
+   {
+       Scanner input = new Scanner(System.in);
+       
+       System.out.println("Hello world!");
+       
+    }
+}
